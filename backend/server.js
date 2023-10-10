0256a92befce377a84a23da566bdd59b64e3b9ea
cristimarcu43 <claudiu.marcu@stud.acs.upb.ro>
2023-10-10 16:32:44 +0300

    Modificare package.json, adaugare port nou

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ monogoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8yloagt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(proces.env.PORT || 5000);
   })
   .catch((err) => {
     console.log(err);
